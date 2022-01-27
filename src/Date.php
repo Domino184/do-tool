27e279f343caa79cbfd87854c2d841cabb8fd551
Domino <m18434900825@163.com>
2022-01-27 10:38:38 +0800

    edit

--- src/Date.php
+++ src/Date.php
@@ -10,17 +10,20 @@
 
 namespace DoTool;
 
+use DateTime;
+use DateTimeZone;
+
 /**
  * 日期时间处理类
  */
 class Date
 {
-    public const YEAR = 31536000;
-    public const MONTH = 2592000;
-    public const WEEK = 604800;
-    public const DAY = 86400;
-    public const HOUR = 3600;
-    public const MINUTE = 60;
+    const YEAR = 31536000;
+    const MONTH = 2592000;
+    const WEEK = 604800;
+    const DAY = 86400;
+    const HOUR = 3600;
+    const MINUTE = 60;
 
     /**
      * 计算两个时区间相差的时长,单位为秒
@@ -30,9 +33,9 @@ class Date
      * [!!] A list of time zones that PHP supports can be found at
      * <http://php.net/timezones>.
      *
-     * @param   string $remote timezone that to find the offset of
-     * @param   string $local  timezone used as the baseline
-     * @param   mixed  $now    UNIX timestamp or date string
+     * @param string $remote timezone that to find the offset of
+     * @param string $local timezone used as the baseline
+     * @param mixed $now UNIX timestamp or date string
      * @return  integer
      */
     public static function offset($remote, $local = null, $now = null)
@@ -43,14 +46,14 @@ class Date
         }
         if (is_int($now)) {
             // Convert the timestamp into a string
-            $now = date(\DateTime::RFC2822, $now);
+            $now = date(DateTime::RFC2822, $now);
         }
         // Create timezone objects
-        $zone_remote = new \DateTimeZone($remote);
-        $zone_local = new \DateTimeZone($local);
+        $zone_remote = new DateTimeZone($remote);
+        $zone_local = new DateTimeZone($local);
         // Create date objects from timezones
-        $time_remote = new \DateTime($now, $zone_remote);
-        $time_local = new \DateTime($now, $zone_local);
+        $time_remote = new DateTime($now, $zone_remote);
+        $time_local = new DateTime($now, $zone_local);
         // Find the offset
         $offset = $zone_remote->getOffset($time_remote) - $zone_local->getOffset($time_local);
         return $offset;
@@ -62,9 +65,9 @@ class Date
      * $span = self::span(60, 182, 'minutes,seconds'); // array('minutes' => 2, 'seconds' => 2)
      * $span = self::span(60, 182, 'minutes'); // 2
      *
-     * @param   int    $remote timestamp to find the span of
-     * @param   int    $local  timestamp to use as the baseline
-     * @param   string $output formatting string
+     * @param int $remote timestamp to find the span of
+     * @param int $local timestamp to use as the baseline
+     * @param string $output formatting string
      * @return  string   when only a single output is requested
      * @return  array    associative list of all outputs requested
      * @from https://github.com/kohana/ohanzee-helpers/blob/master/src/Date.php
@@ -122,45 +125,49 @@ class Date
     /**
      * 格式化 UNIX 时间戳为人易读的字符串
      *
-     * @param    int    Unix 时间戳
-     * @param    mixed $local 本地时间
+     * @param int    Unix 时间戳
+     * @param mixed $local 本地时间
      *
      * @return    string    格式化的日期字符串
      */
     public static function human($remote, $local = null)
     {
-        $timediff = (is_null($local) || $local ? time() : $local) - $remote;
-        $chunks = array(
-            array(60 * 60 * 24 * 365, 'year'),
-            array(60 * 60 * 24 * 30, 'month'),
-            array(60 * 60 * 24 * 7, 'week'),
-            array(60 * 60 * 24, 'day'),
-            array(60 * 60, 'hour'),
-            array(60, 'minute'),
-            array(1, 'second')
-        );
+        $time_diff = (is_null($local) || $local ? time() : $local) - $remote;
+        $tense = $time_diff < 0 ? 'after' : 'ago';
+        $time_diff = abs($time_diff);
+        $chunks = [
+            [60 * 60 * 24 * 365, 'year'],
+            [60 * 60 * 24 * 30, 'month'],
+            [60 * 60 * 24 * 7, 'week'],
+            [60 * 60 * 24, 'day'],
+            [60 * 60, 'hour'],
+            [60, 'minute'],
+            [1, 'second']
+        ];
+        $name = 'second';
+        $count = 0;
 
         for ($i = 0, $j = count($chunks); $i < $j; $i++) {
             $seconds = $chunks[$i][0];
             $name = $chunks[$i][1];
-            if (($count = floor($timediff / $seconds)) != 0) {
+            if (($count = floor($time_diff / $seconds)) != 0) {
                 break;
             }
         }
-        return __("%d {$name}%s ago", $count, ($count > 1 ? 's' : ''));
+        return __("%d $name%s $tense", $count, ($count > 1 ? 's' : ''));
     }
 
     /**
      * 获取一个基于时间偏移的Unix时间戳
      *
-     * @param string $type     时间类型，默认为day，可选minute,hour,day,week,month,quarter,year
-     * @param int    $offset   时间偏移量 默认为0，正数表示当前type之后，负数表示当前type之前
+     * @param string $type 时间类型，默认为day，可选minute,hour,day,week,month,quarter,year
+     * @param int $offset 时间偏移量 默认为0，正数表示当前type之后，负数表示当前type之前
      * @param string $position 时间的开始或结束，默认为begin，可选前(begin,start,first,front)，end
-     * @param int    $year     基准年，默认为null，即以当前年为基准
-     * @param int    $month    基准月，默认为null，即以当前月为基准
-     * @param int    $day      基准天，默认为null，即以当前天为基准
-     * @param int    $hour     基准小时，默认为null，即以当前年小时基准
-     * @param int    $minute   基准分钟，默认为null，即以当前分钟为基准
+     * @param int $year 基准年，默认为null，即以当前年为基准
+     * @param int $month 基准月，默认为null，即以当前月为基准
+     * @param int $day 基准天，默认为null，即以当前天为基准
+     * @param int $hour 基准小时，默认为null，即以当前年小时基准
+     * @param int $minute 基准分钟，默认为null，即以当前分钟为基准
      * @return int 处理后的Unix时间戳
      */
     public static function unixtime($type = 'day', $offset = 0, $position = 'begin', $year = null, $month = null, $day = null, $hour = null, $minute = null)
@@ -170,7 +177,7 @@ class Date
         $day = is_null($day) ? date('d') : $day;
         $hour = is_null($hour) ? date('H') : $hour;
         $minute = is_null($minute) ? date('i') : $minute;
-        $position = in_array($position, array('begin', 'start', 'first', 'front'));
+        $position = in_array($position, ['begin', 'start', 'first', 'front']);
 
         $baseTime = mktime(0, 0, 0, $month, $day, $year);
 
@@ -192,13 +199,13 @@ class Date
                 break;
             case 'month':
                 $_timestamp = mktime(0, 0, 0, $month + $offset, 1, $year);
-                $time = $position ? $_timestamp : mktime(23, 59, 59, $month + $offset, cal_days_in_month(CAL_GREGORIAN, date("m", $_timestamp), date("Y", $_timestamp)), $year);
+                $time = $position ? $_timestamp : mktime(23, 59, 59, $month + $offset, self::days_in_month(date("m", $_timestamp), date("Y", $_timestamp)), $year);
                 break;
             case 'quarter':
                 $_month = date("m", mktime(0, 0, 0, (ceil(date('n', mktime(0, 0, 0, $month, $day, $year)) / 3) + $offset) * 3, $day, $year));
                 $time = $position ?
                     mktime(0, 0, 0, 1 + ((ceil(date('n', $baseTime) / 3) + $offset) - 1) * 3, 1, $year) :
-                    mktime(23, 59, 59, (ceil(date('n', $baseTime) / 3) + $offset) * 3, cal_days_in_month(CAL_GREGORIAN, (ceil(date('n', $baseTime) / 3) + $offset) * 3, $year), $year);
+                    mktime(23, 59, 59, (ceil(date('n', $baseTime) / 3) + $offset) * 3, self::days_in_month((ceil(date('n', $baseTime) / 3) + $offset) * 3, $year), $year);
                 break;
             case 'year':
                 $time = $position ? mktime(0, 0, 0, 1, 1, $year + $offset) : mktime(23, 59, 59, 12, 31, $year + $offset);
@@ -209,4 +216,20 @@ class Date
         }
         return $time;
     }
+
+    /**
+     * 获取指定年月拥有的天数
+     * @param int $month
+     * @param int $year
+     * @return false|int|string
+     */
+    public static function days_in_month($month, $year)
+    {
+        if (function_exists("cal_days_in_month")) {
+            return cal_days_in_month(CAL_GREGORIAN, $month, $year);
+        } else {
+            return date('t', mktime(0, 0, 0, $month, 1, $year));
+        }
+    }
 }
+
